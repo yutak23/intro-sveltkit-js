36fbca3d49bd9ad375d8de6fd9a9284d03317802
yutak23 <yutapilafu@gmail.com>
2023-08-29 08:44:01 +0900

    feat: Part 3: Basic SvelteKit / Loading data / Layout data（+layout.server.jsで共有のデータpropを作成）

--- src/routes/blog/[slug]/+layout.svelte
+++ src/routes/blog/[slug]/+layout.svelte
@@ -1,11 +1,22 @@
 <script>
-	// export let data;
+	export let data;
 </script>
 
 <div class="layout">
 	<main>
 		<slot />
 	</main>
+
+	<aside>
+		<h2>More posts</h2>
+		<ul>
+			{#each data.summaries as { slug, title }}
+				<li>
+					<a href="/blog/{slug}">{title}</a>
+				</li>
+			{/each}
+		</ul>
+	</aside>
 </div>
 
 <style>
