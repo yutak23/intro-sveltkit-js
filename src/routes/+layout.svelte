47b44d24cd9126f809ce8829cf9c986329b8f073
yutak23 <yutapilafu@gmail.com>
2023-08-29 14:30:44 +0900

    feat: Part 3 / Stores / page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	// eslint-disable-next-line import/no-unresolved, import/extensions
+	import { page } from '$app/stores';
+</script>
+
+<nav>
+	<a href="/" aria-current={$page.url.pathname === '/'}> home </a>
+
+	<a href="/about" aria-current={$page.url.pathname === '/about'}> about </a>
+</nav>
+
+<slot />
