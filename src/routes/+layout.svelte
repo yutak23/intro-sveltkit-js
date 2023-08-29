a425760ebed96204c9cbc2e5c0bac362d94f6aac
yutak23 <yutapilafu@gmail.com>
2023-08-29 15:04:04 +0900

    feat: Part 3 / Stores / navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script>
 	// eslint-disable-next-line import/no-unresolved, import/extensions
-	import { page } from '$app/stores';
+	import { page, navigating } from '$app/stores';
 </script>
 
 <nav>
 	<a href="/" aria-current={$page.url.pathname === '/'}> home </a>
 
 	<a href="/about" aria-current={$page.url.pathname === '/about'}> about </a>
+
+	{#if $navigating}
+		navigating to {$navigating.to.url.pathname}
+	{/if}
 </nav>
 
 <slot />
