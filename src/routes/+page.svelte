9e8dfe4df44a83bd292b5edfb3a77d66fcc0c783
yutak23 <yutapilafu@gmail.com>
2023-08-29 14:24:40 +0900

    feat: Part 3 / API routes / Other handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,17 +47,25 @@
 						type="checkbox"
 						checked={todo.done}
 						on:change={async (e) => {
-							// const done = e.currentTarget.checked;
-							// TODO handle change
-							console.log('TODO: handle change', e);
+							const done = e.currentTarget.checked;
+							await fetch(`/todo/${todo.id}`, {
+								method: 'PUT',
+								body: JSON.stringify({ done }),
+								headers: {
+									'Content-Type': 'application/json'
+								}
+							});
 						}}
 					/>
 					<span>{todo.description}</span>
 					<button
 						aria-label="Mark as complete"
-						on:click={async (e) => {
-							// TODO handle delete
-							console.log('TODO: handle delete', e);
+						on:click={async () => {
+							await fetch(`/todo/${todo.id}`, {
+								method: 'DELETE'
+							});
+
+							data.todos = data.todos.filter((t) => t !== todo);
 						}}
 					/>
 				</label>
